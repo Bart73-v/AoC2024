321496c1c4ed35828eb4da4e1e2c640e99f8bd74
bart <bart>
2024-12-02 15:18:07 +0100

    day2 part2 python

---
+++ day2/day2.py
@@ -0,0 +1,31 @@
+def strictly_increasing(reports):
+    for i in range(len(reports)-1):
+        if ( not reports[i] < reports[i+1] ) or ( (reports[i+1] - reports[i]) > 3):
+            return False
+    return True
+
+def strictly_decreasing(reports):
+    for i in range(len(reports)-1):
+        if ( not reports[i] > reports[i+1] ) or ( (reports[i] - reports[i+1]) > 3):
+            return False
+    return True
+        
+
+
+safe_reports = 0
+
+with open("input.txt", "r") as file:
+    for line in file:
+        reports = [int(i) for i in line.split()]
+        safe = False
+        print(reports)
+        if strictly_increasing(reports) or strictly_decreasing(reports):
+            safe_reports+=1
+        else:
+            for i in range(len(reports)):
+                if strictly_increasing(reports[:i] + reports[i+1:]) or strictly_decreasing(reports[:i] + reports[i+1:]):
+                    safe = True
+            if safe:
+                safe_reports+=1
+
+print(safe_reports-1)
