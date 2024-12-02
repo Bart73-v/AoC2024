976ee1ec00e7f78fe3a7ff90348fb1756e0ad5ea
bart <bart>
2024-12-02 11:52:03 +0100

    day2

--- day2/day2.sh
+++ day2/day2.sh
@@ -26,14 +26,31 @@ strictly_dec(){
 }
 
 read_from_file() {
-    while read -a input || [[ -n "$input" ]]; do
+    while read -a input || [[ -n "${input[1]}" ]]; do
         if [[ ${input[0]} -gt ${input[1]} ]]; then 
-            if strictly_dec; then safe_reports+=1 && echo "${input[@]}"; fi
+            if strictly_dec; then safe_reports+=1; fi
         else 
-            if strictly_inc; then safe_reports+=1 && echo "${input[@]}"; fi
+            if strictly_inc; then safe_reports+=1; fi
         fi
 	done < "$input_file"
 }
 
-read_from_file
+read_from_file_p2() {
+    while read -a input || [[ -n "${input[1]}" ]]; do
+        safe=false
+        for (( i - 0; i < ${#input[@]}; i++ )); do
+            input=("${input[@]:$(($i + 1))}")
+            echo "${input[@]}"
+            if [[ ${input[0]} -gt ${input[1]} ]]; then 
+                if strictly_dec; then safe=true; fi
+            else 
+                if strictly_inc; then safe=true; fi
+            fi
+        done
+        if $safe; then safe_reports+=1; fi
+	done < "$input_file"
+}
+
+read_from_file_p2
+#(( safe_reports-=1 )) #something something bash newline
 echo $safe_reports
