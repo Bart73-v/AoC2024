38b79b43527a4dac915199a3e94a3d2a961c63ef
bart <bart>
2024-12-03 10:39:57 +0100

    day3 p2

--- day3/day3.sh
+++ day3/day3.sh
@@ -7,16 +7,40 @@ input_file=$1
 mapfile -t lines < <(sed '/^$/d' "$input_file")
 
 declare -a commands=()
-regex='mul\([0-9]{1,3},[0-9]{1,3}\)'
+regex="mul\([0-9]{1,3},[0-9]{1,3}\)"
+regex_p2="mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\)"
 
-for line in "${lines[@]}"; do
-  matches=$(echo "$line" | grep -oP "$regex")
-  if [[ -n $matches ]]; then
-    while read -r match; do
-      commands+=("$match")
-    done <<< "$matches"
-  fi
-done
+part_one() {
+    for line in "${lines[@]}"; do
+        matches=$(echo "$line" | grep -oP "$regex")
+        if [[ -n $matches ]]; then
+            while read -r match; do
+            commands+=("$match")
+            done <<< "$matches"
+        fi
+    done
+}
+
+part_two() {
+    enabled=true
+    for line in "${lines[@]}"; do
+        matches=$(echo "$line" | grep -oP "$regex_p2")
+        if [[ -n $matches ]]; then
+            while read -r match; do
+                if [[ "$match" = "do()" ]]; then
+                    enabled=true
+                elif [[ "$match" = "don't()" ]]; then
+                    enabled=false
+                elif [[ $enabled = true && "$match" =~ $regex ]]; then
+                    commands+=("$match")
+                fi
+            done <<< "$matches"
+        fi
+    done
+}
+
+#part_one
+part_two
 
 prod=0
 for mul in "${commands[@]}"; do
