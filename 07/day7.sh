3a4def1d79c581c468b8644c78d03eab0761e446
bart <bart>
2024-12-07 14:03:59 +0100

    d7p1

---
+++ 07/day7.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -o pipefail
+
+# Pre-checks
+[[ $# -lt 1 ]] && ( echo "No input file specified"; exit 1 )
+[[ ! -s $1 ]] && ( echo "The input file is empty"; exit 1 )
+
+input_file=$1
+mapfile -t lines < <(sed '/^$/d' "$input_file")
+
+recursive_compute() {
+    value=$1
+    sum=$2
+    read -r -a numbers <<<"${3}"
+    if [[ value -eq sum && -z ${numbers[*]} ]]; then 
+        echo "true"
+    elif [[ -z ${numbers[*]} ]]; then
+        echo "false"
+    else
+        add=$(recursive_compute "$value" "$(( $sum + ${numbers[0]} ))" "${numbers[*]:1}")
+        mul=$(recursive_compute "$value" "$(( $sum * ${numbers[0]} ))" "${numbers[*]:1}")
+        if [[ $add == "true" || $mul == "true" ]]; then echo "true"; else echo "false"; fi
+    fi
+}
+
+calibration_result=0
+i=0
+for line in "${lines[@]}"; do
+    echo "line: $i, total: $calibration_result"
+    ((i++))
+    value=${line%:*}
+    read -r -a numbers <<< "${line#*: }"
+    if [[ "$(recursive_compute "$value" "${numbers[0]}" "${numbers[*]:1}")" == "true" ]]; then
+        calibration_result=$(bc <<< "${calibration_result}+${value}")
+    fi
+done
+
+echo "$calibration_result"
